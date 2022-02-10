60961f876affc3ad8b4ecf65cd688bcedf4a975a
Judori <steblevvlad037@gmail.com>
2022-02-10 12:46:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+
+var selectedTeams = from t in teams
+                    where t.ToUpper().StartsWith("Б") 
+                    orderby t  
+                    select t;
+var selectedTeams2 = teams.Where(t => t.ToUpper().StartsWith("Б")).OrderBy(t => t);
+
+foreach (string s in selectedTeams)
+    Console.WriteLine(s);
+foreach (string s in selectedTeams2)
+    Console.WriteLine(s);
