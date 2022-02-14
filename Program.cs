7e447b8e45258d24b0ab3c66658f30d2e1bf7541
Judori <steblevvlad037@gmail.com>
2022-02-14 12:57:43 +0300

    Добавил перевод карт

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
 ﻿static IEnumerable<string> Suits()
 {
-    yield return "clubs";
-    yield return "diamonds";
-    yield return "hearts";
-    yield return "spades";
+    yield return "трефы";
+    yield return "бубны";
+    yield return "червы";
+    yield return "пики";
 }
 
 static IEnumerable<string> Ranks()
 {
-    yield return "two";
-    yield return "three";
-    yield return "four";
-    yield return "five";
-    yield return "six";
-    yield return "seven";
-    yield return "eight";
-    yield return "nine";
-    yield return "ten";
-    yield return "jack";
-    yield return "queen";
-    yield return "king";
-    yield return "ace";
+    yield return "два";
+    yield return "три";
+    yield return "четыре";
+    yield return "пять";
+    yield return "шесть";
+    yield return "семь";
+    yield return "восемь";
+    yield return "девять";
+    yield return "десять";
+    yield return "валет";
+    yield return "королева";
+    yield return "король";
+    yield return "туз";
 }
 
 
-var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));
+var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Масть = suit, Значение = rank }));
 
 foreach (var c in startingDeck)
 {
